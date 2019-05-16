75346b3782f76b4b5d20cfacf57a256ec3910043
David Porcel <david.porcel@smaato.com>
2019-05-16 18:34:50 +0200

    Create basic package

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name='termsession',
+    version='0.1',
+    packages=[
+        'termsession',
+    ],
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
